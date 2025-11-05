f505c157792aadd23d3d4517d83c9870c7f9a14a
Carolina05-ai <carolinaigonzalezbetancourt@gmail.com>
2025-11-05 17:24:27 -0500

    03_base_comic

---
+++ 03_base_datos_comic/bd.js
@@ -0,0 +1,32 @@
+let comic = {
+  "nombreComic": "Huellas De Ritmo",
+  "numeroCapitulos": 3,
+  "genero": "fantasia/drama",
+  "sinopsis": "narra la historia de Isabel, una niña prodigiosa en el baile que vive en una isla junto a su pueblo. Este lugar enfrenta el desafío de olvidar su herencia musical, lo cual afecta profundamente al guardián del pueblo.",
+  "autores": ["carolina", "jimena", "valentina"],
+  "portadaComic": "./img/portada.jpg",
+  "year": "2025",
+  "capitulos": [
+    {
+      "id": 1,
+      "nombre": "Los ritmos de los maracas y un sueño.",
+      "personajes": 9,
+      "portadaComic": "./img/portada.jpg",
+      "descripcion": "",
+    },
+    {
+      "id": 2,
+      "nombre": "El canto.",
+      "personajes": 5,
+      "portadaComic": "./img/portada.jpg",
+      "descripcion": "",
+    },
+    {
+      "id": 3,
+      "nombre": "Las sombras y el ritual final.",
+      "personajes": 5,
+      "portadaComic": "./img/portada.jpg",
+      "descripcion": "",
+    },
+]
+}
