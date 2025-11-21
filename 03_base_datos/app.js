6f849d4e109dd79cfc3fb45ef0d6e5be0d2eaef1
Carolina05-ai <carolinaigonzalezbetancourt@gmail.com>
2025-11-21 01:05:14 -0500

    base_datos_act_capitulos

---
+++ 03_base_datos/app.js
@@ -0,0 +1,40 @@
+const galeria = document.querySelector('.lista-personajes');
+
+comic.personajes.forEach(char => {
+
+  const div = document.createElement('div');
+  div.classList.add('card');
+  div.innerHTML = `
+  
+  <img src="${char.imagen}" alt="${char.nombre}">
+  <h3>${char.nombre}</h3>
+  <p><strong>Edad:</strong> ${char.edad}</p>
+  <p>${char.descripcion}</p>
+
+  `;
+
+  galeria.appendChild(div);
+
+});
+
+const listaCaps = document.querySelector('.lista-caps');
+
+comic.capitulos.forEach(cap => {
+
+    const div = document.createElement('div');
+    div.classList.add('cap-card');
+
+
+    const imgSrc = cap.imagen ? cap.imagen : "ejemplo.jpg";
+
+    div.innerHTML = `
+        <img src="${imgSrc}" alt="${cap.nombre}">
+        <h3>${cap.nombre}</h3>
+        <p><strong>Personaje:</strong> ${cap.personaje}</p>
+        <p>${cap.descripcion}</p>
+    `;
+
+    listaCaps.appendChild(div);
+});
+
+ 
--- 03_base_datos/bd.js
+++ 03_base_datos/bd.js
@@ -1,33 +1,53 @@
 let comic = {
-  "nombreComic": "Huellas De Ritmo",
-  "numeroCapitulos": 3,
-  "genero": "fantasia/drama",
-  "sinopsis": "narra la historia de Isabel, una niña prodigiosa en el baile que vive en una isla junto a su pueblo. Este lugar enfrenta el desafío de olvidar su herencia musical, lo cual afecta profundamente al guardián del pueblo.",
-  "autores": ["carolina", "jimena", "valentina"],
-  "portadaComic": "./img/portada.jpg",
-  "year": "2025",
+  "nombreComic": "HUELLAS DE RITMO",
+  "numeroCapitulos": "3",
+  "genero": "fantasia-aventura",
+  "sipnosis": "narra la historia de Isabel, una niña prodigiosa en el baile que vive en una isla junto a su pueblo. Este lugar enfrenta el desafío de olvidar su herencia musical, lo cual afecta profundamente al guardián del pueblo",
+  "autores": ["carolina , valentina , jimena"],
+  "personajes": [
+    {
+      "imagen": "shakira.png",
+      "id": "1",
+      "nombre": "Shakira/Isabel",
+      "edad": "10",
+      "descripcion": "Isabel es una niña de 10 años, con una energía inagotable y una pasión desbordante por el baile. Su talento innato la convierte en la mejor bailarina de su aldea, y su espíritu alegre y optimista inspira a todos a su alrededor."
+    },
+    { 
+      
+      "id": "2",
+      "nombre": "lobo",
+      "edad": "55",
+      "descripcion": "Lobo es el guardián del pueblo, un hombre sabio y fuerte que ha dedicado su vida a proteger las tradiciones y la herencia musical de su gente. A pesar de su apariencia ruda, tiene un corazón bondadoso y una profunda conexión con la música y el baile."
+    },
+    {
+      "imagen": "SOMBRAA.png",
+      "id": "3",
+      "nombre": "oscuridad",
+      "edad": "indefinida",
+      "descripcion": "Oscuridad es una entidad misteriosa que representa la amenaza de olvidar la herencia musical del pueblo. Su presencia se siente como una sombra que se extiende sobre la isla, y su objetivo es silenciar la música y el baile que son el alma de la comunidad."
+    }
+  ],
   "capitulos": [
     {
-      "id": 1,
-      "nombre": "Los ritmos de los maracas y un sueño.",
-      "personajes": 9,
-      "portadaComic": "./img/portada.jpg",
-      "descripcion": "",
+      "id": "1",
+      "nombre": "Los ritmos de las maracas y un sueño.",
+      "personaje": "1",
+      "descripcion": [""]
     },
+    
     {
-      "id": 2,
-      "nombre": "El canto.",
-      "personajes": 5,
-      "portadaComic": "./img/portada.jpg",
-      "descripcion": "",
+      "id": "1",
+      "nombre": "el canto.",
+      "personaje": "5",
+      "descripcion": [""]
     },
+    
     {
-      "id": 3,
-      "nombre": "Las sombras y el ritual final.",
-      "personajes": 5,
-      "portadaComic": "./img/portada.jpg",
-      "descripcion": "",
+      "id": "1",
+      "nombre": "las sombras y el ritual final.",
+      "personaje": "5",
+      "descripcion": [""]
     },
-]
-}
-
+     
+  ]
+};
